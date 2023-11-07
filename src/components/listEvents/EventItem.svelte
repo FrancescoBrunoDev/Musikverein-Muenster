b8466bad24a8aff00c3a4edfa004ad1fc63feaa3
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2023-11-07 22:36:17 +0100

    Update EventItem.svelte

--- src/components/listEvents/EventItem.svelte
+++ src/components/listEvents/EventItem.svelte
@@ -19,11 +19,19 @@
 	}
 </script>
 
-<div class={isEventOpen ? 'flex h-fit w-80 flex-shrink-0 flex-col border-2 border-primary dark:border-secondary' : ''}>
+<div
+	class={`w-fit ${
+		isEventOpen
+			? 'flex h-fit flex-shrink-0 flex-col border-2 border-primary dark:border-secondary'
+			: ''
+	}`}
+>
 	<button
 		on:click={() => handleClickEvent()}
-		class={`flex-shrink-0 flex-grow-0 border-primary dark:border-secondary font-bold ${
-			isEventOpen ? 'relative left-0 right-0 top-0 h-fit border-b-2 py-2' : 'h-32 w-24 border-2'
+		class={`flex-shrink-0 flex-grow-0 border-primary font-bold dark:border-secondary ${
+			isEventOpen
+				? 'relative left-0 right-0 top-0 h-fit w-80 border-b-2 py-2'
+				: 'h-32 w-24 border-2'
 		}`}
 		>{date}
 		{#if isEventOpen}
@@ -42,8 +50,8 @@
 		{/if}
 	</button>
 	{#if isEventOpen}
-		<div class="p-2">
-			<div class="text-lg font-bold dark:font-semibold">Performances</div>
+		<div class="w-80 p-2">
+			<div class="w-full text-lg font-bold dark:font-semibold">Performances</div>
 			{#if event.performances}
 				<div class="flex flex-col gap-1 divide-y-2 divide-primary dark:divide-secondary">
 					<EventPerformances {event} />
