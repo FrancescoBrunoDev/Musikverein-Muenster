79e9ee3368c08270bf3f7c87d66c9c382839ca09
Francesco Bruno <59353843+FrancescoBrunoDev@users.noreply.github.com>
2023-11-07 21:50:42 +0100

    Delete .svelte-kit directory

--- .svelte-kit/generated/client/nodes/5.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/timeline/[year]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/timeline/[year]/+page.svelte";
