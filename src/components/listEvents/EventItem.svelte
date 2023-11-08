d71ce1c2af74d45781cfd1c8b3d44707eeba68a6
Francesco Bruno <59353843+FrancescoBrunoDev@users.noreply.github.com>
2023-11-08 08:11:43 +0100

    test dots

--- src/components/listEvents/EventItem.svelte
+++ src/components/listEvents/EventItem.svelte
@@ -1,6 +1,7 @@
 <script>
 	import EventPerformances from './EventPerformances.svelte';
 	import { locationTitles } from '$stores/storeEvents';
+	import { filters } from '$stores/storeFilters';
 	export let event;
 	let isEventOpen = false;
 
@@ -23,7 +24,7 @@
 	class={`w-fit ${
 		isEventOpen
 			? 'flex h-fit flex-shrink-0 flex-col border-2 border-primary dark:border-secondary'
-			: ''
+			: 'flex flex-col gap-2 justify-center'
 	}`}
 >
 	<button
@@ -34,6 +35,35 @@
 				: 'h-32 w-24 border-2'
 		}`}
 		>{date}
+		<div>
+			{#each event.performances as performance}
+			{#if $filters.or.some((filter) => filter.entity === 'composer' && performance.composers && filter.id == performance.composers[0].person) || $filters.or.some((filter) => filter.entity === 'work' && filter.id == performance.work) || $filters.or.some((filter) => filter.entity === 'location' && filter.id == performance.location) || $filters.or.some((filter) => filter.entity === 'person' && performance.persons.some((person) => filter.id == person.person))}
+				<div class="flex items-center gap-1">
+
+						{#each $filters.or as filter}
+							{#if filter.entity === 'composer' && performance.composers && filter.id == performance.composers[0].person}
+								<Circle class="flex-shrink-0" fill={filter.color} size={10} stroke-opacity={0} />
+							{/if}
+							{#if filter.entity === 'work' && filter.id == performance.work}
+								<Circle class="flex-shrink-0" fill={filter.color} size={10} stroke-opacity={0} />
+							{/if}
+							{#if filter.entity === 'location' && filter.id == performance.location}
+								<Circle class="flex-shrink-0" fill={filter.color} size={10} stroke-opacity={0} />
+							{/if}
+							{#if filter.entity === 'person'}
+								{#each performance.persons as person}
+									{#if filter.id == person.person}
+										<Circle class="flex-shrink-0" fill={filter.color} size={10} stroke-opacity={0} />
+									{/if}
+								{/each}
+							{/if}
+						{/each}
+			
+				</div>
+			{:catch error}
+				<div>Error: {error.message}</div>
+			{/each}
+		</div>
 		{#if isEventOpen}
 			<br />
 			<span class="text-sm dark:font-semibold">
