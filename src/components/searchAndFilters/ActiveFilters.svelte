7edc99888facd25d8c9371642162f1f76d300605
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2023-11-19 21:09:44 +0100

    filtersShareButton moved

--- src/components/searchAndFilters/ActiveFilters.svelte
+++ src/components/searchAndFilters/ActiveFilters.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { filters } from '$stores/storeFilters';
 	import FilterGroup from '$components/searchAndFilters/FilterGroup.svelte';
-	import FiltersShareButton from '$components/FiltersShareButton.svelte';
+	import FiltersShareButton from '$components/searchAndFilters/FiltersShareButton.svelte';
 	type GroupedFilters = { [key: string]: Filter[] };
 
 	let groupedFiltersOR: GroupedFilters;
