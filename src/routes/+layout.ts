d438814b4ad7599bdaf74df1d0809b1d75628381
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-04-28 18:17:25 +0200

    try Node server

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,7 +4,6 @@ import { setLocale } from '$lib/i18n/i18n-svelte';
 import { detectLocale } from '$lib/i18n/i18n-util';
 import { loadLocaleAsync } from '$lib/i18n/i18n-util.async';
 import { sessionStorageDetector, navigatorDetector } from 'typesafe-i18n/detectors';
-export const prerender = true;
 
 export const load: LayoutLoad = async (event) => {
   if (browser) {
