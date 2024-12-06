6a24b3427925251f5eb78d66b064407810cf293c
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-12-06 19:07:50 +0100

    event use runes

--- src/components/listEvents/Event.svelte
+++ src/components/listEvents/Event.svelte
@@ -5,86 +5,83 @@
 	import { filters, filteredEvents } from '$stores/storeFilters';
 	import { Circle, FileInput } from 'lucide-svelte';
 	import { fly } from 'svelte/transition';
-	export let eventUid: number;
+	let { event }: { event: EventItem } = $props();
 
-	let event: EventItem;
-	let date: string;
-	let isEventOpen = false;
+	let date: string = $derived.by(() => calculateDate(event));
+	let isEventOpen = $state(false);
 	let filtersArrayWithCounter: {
 		[key: string]: {
 			counter: number;
 			color: string;
 		};
-	} = {};
+	} = $derived.by(() => countFilters());
 
-	$: {
-		if ($filteredEvents) {
-			Object.keys($filteredEvents).forEach((year) => {
-				$filteredEvents[year as keyof Events].forEach((eventItem: EventItem) => {
-					if (eventItem.uid === eventUid) {
-						event = eventItem;
-					}
-				});
-			});
-			let dateStr = event?.dates[0].date;
-			if (dateStr && dateStr !== '00.00') {
-				// handle date format 00.00 or day undefined
-				if (dateStr.endsWith('-00')) {
-					let dateObj = new Date(dateStr.slice(0, -2) + '01');
-					date = dateObj.toLocaleDateString('it-IT', { month: '2-digit' }) + '.?';
-				} else {
-					let dateObj = new Date(dateStr);
-					date = dateObj.toLocaleDateString('it-IT', { day: '2-digit', month: '2-digit' });
-					date = date.split('/').join('.');
-				}
+	function calculateDate(event: EventItem) {
+		let dateStr = event?.dates[0].date;
+		if (dateStr && dateStr !== '00.00') {
+			// handle date format 00.00 or day undefined
+			if (dateStr.endsWith('-00')) {
+				let dateObj = new Date(dateStr.slice(0, -2) + '01');
+				return dateObj.toLocaleDateString('it-IT', { month: '2-digit' }) + '.?';
 			} else {
-				date = '?';
+				let dateObj = new Date(dateStr);
+				let formattedDate = dateObj.toLocaleDateString('it-IT', {
+					day: '2-digit',
+					month: '2-digit'
+				});
+				return formattedDate.split('/').join('.');
 			}
+		} else {
+			return '?';
 		}
-		filtersArrayWithCounter = {};
-		countFilters();
 	}
 
 	function handleClickEvent() {
 		isEventOpen = !isEventOpen;
+		console.log(isEventOpen);
 		if (isEventOpen) {
 			getTitles(event);
 		}
 	}
 
 	function countFilters() {
+		const filtersMap = new Map();
+
 		const incrementCounter = (id: number, condition: boolean) => {
 			if (condition) {
-				filtersArrayWithCounter[id].counter++;
+				const filter = filtersMap.get(id);
+				if (filter) {
+					filter.counter++;
+				}
 			}
 		};
 
-		Object.values($filters).forEach((method) => {
-			method.forEach((filter) => {
-				if (!filtersArrayWithCounter.hasOwnProperty(filter.id)) {
-					filtersArrayWithCounter[filter.id] = {
+		Object.values($filters).forEach((methods) => {
+			methods.forEach((filter) => {
+				if (!filtersMap.has(filter.id)) {
+					filtersMap.set(filter.id, {
 						counter: 0,
 						color: filter.color || ''
-					};
+					});
 				}
 
 				if (!event.performances) return;
 
-				switch (filter.entity) {
+				const { entity, id } = filter;
+				const filterId = Number(id);
+
+				switch (entity) {
 					case 'composer':
 					case 'work':
 					case 'person':
 						event.performances.forEach((performance) => {
-							if (filter.entity === 'composer' && performance.composers) {
-								incrementCounter(
-									filter.id,
-									performance.composers && filter.id == performance.composers[0].person
-								);
-							} else if (filter.entity === 'work') {
-								incrementCounter(filter.id, performance && filter.id == performance.work);
-							} else if (filter.entity === 'person' && performance.persons) {
+							if (entity === 'composer' && performance.composers) {
+								incrementCounter(filterId, filter.id == performance.composers[0].person);
+							} else if (entity === 'work') {
+								incrementCounter(filterId, filter.id == performance.work);
+							} else if (entity === 'person' && performance.persons) {
 								performance.persons.forEach((person) => {
-									incrementCounter(filter.id, filter.id == person.person);
+									incrementCounter(filterId, filter.id == person.person);
 								});
 							}
 						});
@@ -92,7 +89,7 @@
 					case 'corporation':
 						if (event.corporations) {
 							event.corporations.forEach((corporation) => {
-								incrementCounter(filter.id, filter.id == corporation.corporation);
+								incrementCounter(filterId, filter.id == corporation.corporation);
 							});
 						}
 						break;
@@ -100,7 +97,8 @@
 			});
 		});
 
-		filtersArrayWithCounter;
+		const filtersArrayWithCounter = Object.fromEntries(filtersMap);
+		return filtersArrayWithCounter;
 	}
 </script>
 
@@ -112,7 +110,7 @@
 	}`}
 >
 	<button
-		on:click={() => handleClickEvent()}
+		onclick={() => handleClickEvent()}
 		class={`z-10 flex-shrink-0 flex-grow-0 font-bold transition-all duration-100 ease-in-out ${
 			isEventOpen ? 'relative left-0 right-0 top-0 h-fit w-80 py-2' : 'h-32 w-24'
 		}`}
@@ -144,7 +142,7 @@
 				{#if event.locations}
 					{#each event.locations as location}
 						{#await getTitleString(location.location, 'location')}
-							<div />
+							<div>loading</div>
 						{:then title}
 							<div transition:fly={{ y: 10, duration: 100, delay: 200 }}>{title}</div>
 						{:catch error}
