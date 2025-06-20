7683584e67997ed25b2b92244ec9f0a21a790790
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2025-06-20 12:10:36 +0200

    fix: update import path for joinEventByYear in +page.server.ts

--- src/routes/database/[fo]/[fa]/[fn]/+page.server.ts
+++ src/routes/database/[fo]/[fa]/[fn]/+page.server.ts
@@ -1,4 +1,4 @@
-import { joinEventByYear } from '$lib/dataMusiconn.server';
+import { joinEventByYear } from '$databaseMusiconn/lib/dataMusiconn.server';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
